bde713370d4cb0601ac4d24bd94b3bbc5cd2fd26
Loadmap <130910770+Loadmap@users.noreply.github.com>
2024-05-13 21:49:43 +0900

    Add files via upload

---
+++ KoreaIT/math_tools_2024_05_13.py
@@ -0,0 +1,22 @@
+# 변수
+
+pi = 3.141592
+
+# 함수
+
+def add(a, b) :
+    return a + b
+
+def subtract(a, b) :
+    return a - b
+
+# 클래스
+
+class Circle :
+
+    def __init__(self, radius) :
+        self.radius = radius
+
+    def area(self) :
+        return pi * self.radius * self.radius
+
